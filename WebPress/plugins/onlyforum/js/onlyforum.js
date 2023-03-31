b0271599ece5be0314d43a586969f7adb11ceef9
surveybuilderadmin <surveybuildersbot@gmail.com>
2023-03-31 12:21:36 -0400

    Easy download

--- WebPress/plugins/onlyforum/js/onlyforum.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	const btn = $('#addtopicbtn');
-	btn.attr('data-bs-target', '');
-	btn.attr('class', btn.attr('class').replace('btn-success', 'btn-danger'));
-	btn.children('i').attr('class', 'fa-solid fa-lock');
-});
--- WebPress/plugins/reactions/js/reactions.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$('#reactionName').on('input',function(){
-		$('#reactionIcon').val(($('#reactionName').val()!=='' ? $('#reactionName').val().replaceAll(' ','-')+'.png' : ''));
-	});
-});
