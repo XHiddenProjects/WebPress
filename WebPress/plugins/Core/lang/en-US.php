8eb67fee1cafde046a087105fb7b743429236029
surveybuilderadmin <surveybuildersbot@gmail.com>
2023-03-31 12:30:08 -0400

    core

--- WebPress/plugins/Core/lang/en-US.php
+++ WebPress/plugins/Core/lang/en-US.php
@@ -3,7 +3,7 @@ $plugin='Core';
 $lang[$plugin.'_name'] = 'Core';
 $lang[$plugin.'_desc'] = 'Easy way to run WebPress, activates and creates editors and etc.';
 $lang[$plugin.'_author'] = 'SurveyBuilderTeams';
-$lang[$plugin.'_updated'] = '03-27-2023';
+$lang[$plugin.'_updated'] = '03-31-2023';
 $lang[$plugin.'_homepage'] = 'https://github.com/surveybuilderteams/webpress';
 $lang['checkPolicy']='Check out the WebPress policy on the <a href="/'.MAINDIR.'/dashboard.php/docs#policy">documentation</a>';
 ?>
